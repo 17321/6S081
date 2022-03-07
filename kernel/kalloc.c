c3503cd68a053a215ee040c9b0dd076305854ab2
17321 <1021237577@qq.com>
2022-03-07 16:02:06 +0800

    20220307

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+getFreeMemBytes(void) {
+  uint64 bytes=0;
+  struct run *p=kmem.freelist;
+
+  acquire(&kmem.lock);
+  while(p){
+    bytes+=PGSIZE;
+    p=p->next;
+  }
+  release(&kmem.lock);
+
+  return bytes;
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,4 +95,32 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+
+uint64
+sys_trace(void)
+{
+  int mask;
+  if(argint(0, &mask) < 0)
+    return -1;
+  myproc()->mask=mask;
+  return 0;  // not reached
+}
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 dstAddr;
+  if(argaddr(0, &dstAddr) < 0)
+    return -1;
+
+  struct sysinfo tmp;
+  tmp.freemem=getFreeMemBytes();
+  tmp.nproc=getProcNum();
+
+  if(copyout(myproc()->pagetable, dstAddr, (char *)&tmp, sizeof(tmp)) < 0)
+    return -1;
+
+  return 0;
 }
